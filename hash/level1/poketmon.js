a39a171eed26b5429f7c6f032b29155bec8cdc11
dohuns <gkgk586@gmail.com>
2024-04-03 22:11:54 +0900

    hash > level1 > Set으로 풀이

--- hash/level1/poketmon.js
+++ hash/level1/poketmon.js
@@ -1,15 +1,8 @@
 function solution(nums) {
-  const newMap = new Map();
+  const numLength = Math.floor(nums.length / 2);
+  const newSet = new Set(nums);
 
-  nums.forEach(num => {
-    newMap.set(num, newMap.get(num) ? newMap.get(num) + 1 : 1)
-  })
-
-  if (newMap.size > nums.length / 2) {
-    return  Math.floor(nums.length /2 )
-  } 
-
-  return newMap.size
+  return newSet.size > numLength ? numLength : newSet.size
 }
 
 console.log(solution([1,2,3,4,4]))
