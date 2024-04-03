61e1ca4a368b877498a64a2149c1a70566b46fcb
dohuns <gkgk586@gmail.com>
2024-04-03 21:49:02 +0900

    level2 : Map을 이용해서 품(더 빠름)

--- level2/FunctionDevelopment.js
+++ level2/FunctionDevelopment.js
@@ -1,28 +1,22 @@
 function solution(participants, completions) {
-    var answer = '';
-    
-    participants.sort();
-    completions.sort();
-    
-    participants.some((participant, index) => {
-        if(participant !== completions[index]) {
-            answer = participant
-            return true;
-        }
-    });
-  return answer;
+  var answer = '';
 
-// 테스트 1 〉	통과 (0.06ms, 33.4MB)
-// 테스트 2 〉	통과 (0.05ms, 33.4MB)
-// 테스트 3 〉	통과 (0.32ms, 33.5MB)
-// 테스트 4 〉	통과 (1.00ms, 33.7MB)
-// 테스트 5 〉	통과 (0.60ms, 33.7MB)
-// 테스트 6 〉	통과 (0.05ms, 33.4MB)
-// 테스트 7 〉	통과 (0.05ms, 33.6MB)
+  const newMap = new Map();
   
-// 테스트 1 〉	통과 (42.26ms, 46.8MB)
-// 테스트 2 〉	통과 (87.51ms, 51.9MB)
-// 테스트 3 〉	통과 (102.53ms, 56.7MB)
-// 테스트 4 〉	통과 (108.38ms, 58.8MB)
-// 테스트 5 〉	통과 (90.09ms, 56.8MB)
+  participants.forEach(participant => {
+    newMap.set(participant, newMap.get(participant) ? newMap.get(participant) + 1 : 1)
+  })
+
+  completions.forEach(completion => {
+    newMap.set(completion, newMap.get(completion) - 1)
+  })
+
+  participants.some(participant => {
+    if (newMap.get(participant) === 1) {
+      answer = participant
+      return true
+    }
+  })
+
+  return answer
 }
