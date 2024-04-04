d8f40bad954860675fe5c15bf629be87bff95dc2
dohuns <gkgk586@gmail.com>
2024-04-05 00:29:26 +0900

    queue > level2 >기능개발 >조건이 처리 방식 변경

--- stack&queue/level2/functionDevelopment.js
+++ stack&queue/level2/functionDevelopment.js
@@ -1,25 +1,20 @@
 function solution(progresses, speeds) {
-  var answer = [];
+  var answer = [0];
   
   let cnt = 0
-  let workDay = 0
+  let maxDay = 0
 
   progresses.forEach((progress, index) => {
-    if (workDay * speeds[index] + progress < 100) {
-      if (cnt !== 0) {
-        answer.push(cnt)
-      }
-    
-      if (index === progresses.length - 1) answer.push(1)
-      
-      workDay = Math.ceil((100 - progress) / speeds[index])
-      cnt = 1
+    const workDay = Math.ceil((100 - progress) / speeds[index])
+    if(maxDay === 0) maxDay = workDay
+    if (workDay <= maxDay) {
+      answer[cnt] += 1
     } else {
-      cnt += 1
-      if(index === progresses.length - 1) answer.push(cnt)
+      maxDay = workDay
+      answer[++cnt] = 1
     }
   })
-    return answer;
+  return answer;
 }
 
 console.log(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
